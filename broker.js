44a8883bdab2a9ad6c03337e353877c67d772a70
aranajhonny <jhonnyjosearana@gmail.com>
2019-08-13 21:36:59 -0500

    ..

--- broker.js
+++ broker.js
@@ -8,42 +8,17 @@ client.connect();
 
 (async () => {
 	try {
-		const NODE_ID = `${Math.floor(Math.random() * 5)}`;
+		const NODE_ID = `${Math.floor(Math.random() * 2)}`;
 		const PROGRAM_ID = 5; // STATIC FOR TEST // `${Math.floor(Math.random() * 8)}`;
 
-		const getLock = await lock(PROGRAM_ID);
-		if (getLock) {
-			console.log('lock acquired');
-			const query = 'INSERT INTO assignment ("nodeId", "programId") VALUES ($1, $2);';
-			const values = [NODE_ID, PROGRAM_ID];
-			console.log(values);
+		const query = 'INSERT INTO assignment ("nodeId", "programId") VALUES ($1, $2);';
+		const values1 = ['0', PROGRAM_ID];
+		const values2 = ['1', PROGRAM_ID];
+		// console.log(values);
 
-			client.query(query, values);
-			// remove lock for other nodes
-			setTimeout(async () => {
-				await unlock(PROGRAM_ID);
-				console.log('unlock');
-			}, 5000);
-		}
+		client.query(query, values1);
+		client.query(query, values2);
 	} catch (err) {
 		console.log(err);
 	}
 })();
-
-function lock(id) {
-	try {
-		const query = 'SELECT pg_advisory_lock("lockId") AS success  FROM "programInstance" WHERE "lockId" = $1';
-		return client.query(query, [id]);
-	} catch (e) {
-		console.log(e);
-	}
-}
-
-function unlock(id) {
-	try {
-		const query = 'SELECT pg_advisory_unlock("lockId") AS success  FROM "programInstance" WHERE "lockId" = $1';
-		return client.query(query, [id]);
-	} catch (e) {
-		console.log(e);
-	}
-}
--- node.js
+++ node.js
@@ -7,18 +7,39 @@ const client = new Client({
 client.connect();
 
 const assignments = new Set();
-const NODE_ID = `${Math.floor(Math.random() * 4)}`; // 0..3
+const NODE_ID = `${Math.floor(Math.random() * 2)}`; 
 
 console.log('> Node id: ' + NODE_ID);
 
-client.on('notification', function(msg) {
+client.on('notification', async function(msg) {
 	let payload = JSON.parse(msg.payload);
 
-	if (payload.data.node_id === NODE_ID) {
-		assignments.add(payload.data.program_id);
+	if (payload.data.nodeId === NODE_ID) {
+		assignments.add(payload.data.programId);
+		// programa esta asignado a este node 
+		await lock(payload.data.programId);
+
+		// remove blocking for
+		// other nodes to use the program
+		setTimeout(async () => {
+			await unlock(payload.data.programId);
+			console.log('unlock');
+		}, 8000);
 	}
 	// Programs assigned to this node
 	console.log(assignments);
 });
 
 client.query('LISTEN events');
+
+function lock(id) {
+	const query =
+		'SELECT pg_advisory_lock("lockId") AS success  FROM "programInstance" WHERE "lockId" = $1';
+	return client.query(query, [id]);
+}
+
+function unlock(id) {
+	const query =
+		'SELECT pg_advisory_unlock("lockId") AS success  FROM "programInstance" WHERE "lockId" = $1';
+	return client.query(query, [id]);
+}
