2d4e9ffd09ff2feea7c75706e34a000db636786b
aranajhonny <jhonnyjosearana@gmail.com>
2019-08-22 15:11:27 -0500

    ..

--- node.js
+++ node.js
@@ -7,13 +7,13 @@ const client = new Client({
 client.connect();
 
 const assignments = new Set();
-const NODE_ID = `${Math.floor(Math.random() * 2)}`;
+const NODE_ID = '1' //`${Math.floor(Math.random() * 2)}`;
 
 console.log('> Node id: ' + NODE_ID);
 
 client.on('notification', async function(msg) {
   let payload = JSON.parse(msg.payload);
-
+  console.log(payload)
   if (payload.data.nodeId === NODE_ID) {
     assignments.add(payload.data.programId);
 
@@ -28,27 +28,28 @@ client.on('notification', async function(msg) {
     // remove blocking for
     // other nodes to use the program
     setTimeout(async () => {
-    await unlock(payload.data.nodeId, payload.data.programId);
-    console.log(`node_id ${payload.data.nodeId} UNLOCK programInstance ${payload.data.programId} ${(new Date().toString()).replace(/ \w+-\d+ \(.*\)$/,"")}`);
-
-    }, 5000);
+      await unlock(payload.data.nodeId, payload.data.programId);
+      console.log(`node_id ${payload.data.nodeId} UNLOCK programInstance ${payload.data.programId} ${(new Date().toString()).replace(/ \w+-\d+ \(.*\)$/,"")}`);
+    }, 8000);
   }
   // Programs assigned to this node
   // console.log(assignments);
 });
 
+client.on('error', err => {
+   process.exit(1);
+})
+
 client.query('LISTEN events');
 
 function lock(node, program) {
-  
-
   const query =
-    'SELECT pg_advisory_lock("lockId") FROM "programInstance" WHERE "lockId" = $1';
+    'SELECT pg_advisory_lock("id") FROM "programInstance" WHERE "id" = $1';
   return client.query(query, [program]);
 }
 
 function unlock(node, program) {
   const query =
-    'SELECT pg_advisory_unlock("lockId") FROM "programInstance" WHERE "lockId" = $1';
+    'SELECT pg_advisory_unlock("id") FROM "programInstance" WHERE "id" = $1';
   return client.query(query, [program]);
 }
